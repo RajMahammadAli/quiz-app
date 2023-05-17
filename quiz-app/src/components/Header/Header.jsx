fd26040857e26073d371fa8346fbd309736ec9b9
RajMahammadAli <ali.rc.raj@gmail.com>
2023-05-17 16:47:53 +0600

    landing page edited

--- quiz-app/src/components/Header/Header.jsx
+++ quiz-app/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <>
       <div className="max-w-[1200px] mx-auto bg-blue-500 h-[60px]">
         <Navbar />
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 mt-12">
           <div className="w-[500px]">
             <img src={registration} alt="" className="object-cover" />
           </div>
--- quiz-app/src/components/Registration/Registration.jsx
+++ quiz-app/src/components/Registration/Registration.jsx
@@ -1,6 +1,6 @@
 const Registration = () => {
   return (
-    <div className="w-full h-full bg-blue-400">
+    <div className="w-full h-full bg-blue-500">
       <div className="text-center">
         <h1 className="text-2xl text-white font-extrabold p-4">
           Welcome To Quiz App
@@ -10,7 +10,7 @@ const Registration = () => {
         <h4 className="text-xl text-center text-white font-bold p-4">
           Register Here
         </h4>
-        <form className="p-4 flex flex-col items-center">
+        <form className="p-4 flex flex-col items-center text-white">
           <div className="m-2">
             <label className="pr-[35px]">Name:</label>
             <input type="text" className="rounded" required />
@@ -30,7 +30,7 @@ const Registration = () => {
           </div>
         </form>
         <div className="w-full text-center mx-auto">
-          <h2>Or Register With</h2>
+          <h2 className="text-white">Or Register With</h2>
           <div className="w-[300px] p-4 mx-auto">
             <button className="border w-full bg-white text-blue-500 font-bold rounded">
               Google
